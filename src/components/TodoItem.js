bd77687a4c90f71f53a397d2bd3171bf62f530e3
Nonwuorah <allaboutnedu@gmail.com>
2024-04-13 10:35:06 +0100

    Updated Check and Strickthrough to TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,22 +3,36 @@ import React, { Component } from "react";
 export default class TodoItem extends Component {
   //The TodoItem component is a single item in the list. The TodoItem component is imported into the TodoList component.
   state = {
-    showConfirm: false
+    showConfirm: false,
+    checked: false // New state for checkbox
   };
 
   toggleConfirm = () => {
     this.setState({ showConfirm: !this.state.showConfirm });
   };
 
+  toggleCheckbox = () => {
+    this.setState({ checked: !this.state.checked });
+  };
+
   render() {
     //The title is destructured from the props.
     const { title, handleDelete, handleEdit } = this.props;
-    const { showConfirm } = this.state;
+    const { showConfirm, checked } = this.state;
+
+    const styles = {
+      textDecoration: checked ? "line-through" : "none",
+    };
 
     return (
       //The TodoItem component is a single item in the list. The TodoItem component is imported into the TodoList component.
       <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
-        <h6>{title}</h6>
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={this.toggleCheckbox}
+        />
+        <h6 style ={styles}>{title}</h6>
         <div className="todo-icon">
           <span className="mx-2 text-success" onClick={handleEdit}>
             <i className="fas fa-pen"></i>
